f4998113f76576af524cbf2a3843c3196aad1195
gmorneau <gregmorneau@yahoo.com>
2017-01-24 12:38:28 -0600

    Update index.js

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,20 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+function Hamming() {
+    this.compute = function(strand1, strand2) {
+        if (strand1.length != strand2.length) {
+            throw('DNA strands must be of equal length.');
+        } else {
+            var totalDistance = 0;
+            for (var i=0; i<strand1.length; i++) {
+                if (strand1.charAt(i) !== strand2.charAt(i)) {
+                    totalDistance++;
+                }
+            }
+            return totalDistance;
+        }
+    }
+};
 
 module.exports = Hamming;
